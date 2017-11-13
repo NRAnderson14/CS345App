16b161b58ebe8e6d95ed6603aa87dd72effe7411
Nathan Anderson <nranderson14@winona.edu>
2017-11-12 20:05:53 -0600

    Initial Commit

---
+++ CS345AppBackend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS345AppBackend
+//
+//  Created by Anderson, Nathan R on 11/12/17.
+//  Copyright © 2017 Anderson, Nathan R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
